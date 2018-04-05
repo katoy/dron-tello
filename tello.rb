bad5c33a6b1422a6f72f52b8b3c9a7353f9410e5
kato_youichi <youichikato@gmail.com>
2018-04-06 01:25:24 +0900

    using thread.

--- tello.rb
+++ tello.rb
@@ -5,19 +5,29 @@ class Tello
     @udps = UDPSocket.open
     @sockaddr = Socket.pack_sockaddr_in(8889, '192.168.10.1')
     @udps.bind('0.0.0.0', 9000)
+
+    @th = Thread.start do
+      loop do
+        begin
+          sleep(0.1)
+          puts "#   response #{@udps.recv(1518)}"
+        rescue StandardError => e
+          puts "#   error #{e.message}"
+        end
+      end
+    end
   end
 
   def close
-    p '# close Telo'
+    puts '# close Telo'
     @udps.send('land', 0, @sockaddr)
+    Thread.kill(@th)
     @udps.close
   end
 
   def send_command(message)
-    p "# send #{message}"
+    puts "# send #{message}"
     @udps.send(message, 0, @sockaddr)
-    sleep(0.1)
-    p "#   response #{@udps.recv(1518)}"
   end
 end
 
